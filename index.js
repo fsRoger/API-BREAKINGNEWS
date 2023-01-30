aa132e2abeab4882cd2f6fc8487913e24e5876fa
fsRoger <fsroger@outlook.com>
2023-01-30 09:20:52 -0300

    added  the software architecture

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-const express = require('express')
-const app = express()
+//const { json } = require("express");
+//app.use(json);
+const express = require("express");
+const userRoute = require("./src/routes/user.route")
 
-app.get('/', (req, res) => {
-    //funcao de callback, precisa ser funcao anonima, sem nome definido, apenas function(){},pode ser escrita arrow function ()=>{}
-    res.send('Hello World')
-})
+const app = express();
 
-app.listen(3000)
+app.use("/soma", userRoute);
+
+//ROTA
+//Method HTTP - CRUD (CREATE,READ,UPDATE,DELETE)
+//GET - Pega uma info
+//POST - Cria uma info
+//PUT - Altera toda info
+//PATH - Altera parte da info 
+//DELETE - Apaga uma info
+
+//NAME - Um identificador da rota
+
+//Function (Callback) - Responsavel por execultar algum comando
+
+
+//funcao de callback, precisa ser funcao anonima, sem nome definido, apenas function(){},pode ser escrita arrow function ()=>{}
+//app.get("/soma", (req, res) => {
+//  const soma = 1 + 1;
+
+//res.json(soma)
+// res.send({soma: soma})
+//});
+
+app.listen(3000);
