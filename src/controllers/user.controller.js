6113aa230c47da33d3267e3b3b5d2d37b9660378
fsRoger <fsroger@outlook.com>
2023-02-23 12:48:39 -0300

    alteracao no banco

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,15 +1,17 @@
+const { default: mongoose } = require("mongoose");
 const userService = require("../service/user.service")
 const create = async (req, res) => {
-    const {name, username, email, password, avatar, background} = req.body; 
+    const { name, username, email, password, avatar, background } = req.body;
 
-    if (!name || !username || !email || !password || !avatar || !background){
-        res.status(400).send({message: "submit all fields for registration"});
+
+    if (!name || !username || !email || !password || !avatar || !background) {
+        res.status(400).send({ message: "submit all fields for registration" });
     }
 
-    const user = await userService.create(req.body);
+    const user = await userService.createService(req.body);
 
-    if(!user){
-        return res.status(400).send({message: "error creating user"});
+    if (!user) {
+        return res.status(400).send({ message: "error creating user" });
     }
 
     res.status(201).send({
@@ -26,4 +28,30 @@ const create = async (req, res) => {
     });
 };
 
-module.exports = { create };
+const findAll = async (req, res) => {
+    const users = await userService.findAllService();
+
+    if (users.legth === 0) {
+        return res.status(400).send({ message: "there are no registered users" });
+    }
+
+    res.send(users)
+}
+const findById = async (req, res) => {
+    const id = req.params.id
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).send({ message: "invalid id" });
+    }
+
+    const user = await userService.findByIdService(id);
+
+    if (!user) {
+        return res.status(400).send({ message: "user not found" });
+    }
+
+    res.send(user);
+
+};
+
+module.exports = { create, findAll, findById };
